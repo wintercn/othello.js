30914ffa7d1866bd422a7c2138398df6822a3c6b
winter <csf178@gmail.com>
2012-06-27 19:10:46 +0800

    init

---
+++ src/AI.js
@@ -0,0 +1,307 @@
+
+var AI = {};
+
+new function(){
+
+AI.Pattern= pattern;
+
+var directions=[-11,-10,-9,-1,1,9,10,11];
+function pattern()
+{
+	for(var i=0;i<100;i++)this[i]=0;
+	this[54]=this[45]=1;this[55]=this[44]=2;
+	this.divergence=0;
+	this.color=1;
+	this.moves=0;
+
+	var stableProto = [	
+		3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+	]	
+	this.load=function(arr)
+	{
+		for(var y=1;y<=8;y++)
+		{
+			for(var x=1;x<=8;x++)
+			{
+				this[y*10+x]=arr[y-1][x-1];		 
+			}
+
+		}
+	}
+
+	this.pass=function()
+	{
+		for(var y=1;y<=8;y++)
+		{
+			for(var x=1;x<=8;x++)
+			{
+				if(this[y*10+x]==0) 
+				{
+					if(this.move(x,y,this.color))
+					{
+						return false;
+					}
+				}				 
+			}
+
+		}
+		//alert("pass");
+		this.color = 3 - this.color;
+		return true;
+	}
+
+
+	this.clone=function()
+	{
+		function pattern(){}
+		pattern.prototype=this;
+		return new pattern();
+	}
+	this.toString=function()
+	{
+		var icon=[" ","*","o"]
+		var r="";
+		for(var y=1;y<=8;y++)
+		{
+			for(var x=1;x<=8;x++)
+			{
+				r+=icon[this[y*10+x]]+" ";
+				//r+=stableDiscs[y*10+x]+" ";
+			}
+			r+="\n";
+		}
+		return r+this.exact();
+	}
+	
+	this.exact=function()
+	{
+		var r=[0,0,0];
+		for(var y=1;y<=8;y++)
+		{
+			for(var x=1;x<=8;x++)
+			{
+				r[this[y*10+x]]++;
+			}
+		}
+		if(r[this.color]==0) return -64;
+		if(r[3-this.color]==0) return 64;
+		return r[this.color]-r[3-this.color];
+	}
+
+	this.calculate=function()
+	{
+		var r=[0,0,0];
+		var r=this.divergence;		
+		if(this[11])r+=((this[11]==this.color)?1:-1)*30;
+		else if(this[22])r+=((this[22]==this.color)?-1:1)*25;
+		if(this[18])r+=((this[18]==this.color)?1:-1)*30;
+		else if(this[27])r+=((this[27]==this.color)?-1:1)*25;
+		if(this[81])r+=((this[81]==this.color)?1:-1)*30;
+		else if(this[72])r+=((this[72]==this.color)?-1:1)*25;
+		if(this[88]){r+=((this[88]==this.color)?1:-1)*30;}
+		else if(this[77])r+=((this[77]==this.color)?-1:1)*25;
+		//var color = this.color;
+		/*var stableDiscs=stableProto.slice();
+		
+		var queue = [];
+
+		if(this[11]!=0) queue.push([11,this[11]]);
+		if(this[18]!=0) queue.push([18,this[18]]);
+		if(this[81]!=0) queue.push([81,this[81]]);
+		if(this[88]!=0) queue.push([88,this[88]]);
+		while(queue.length)
+		{
+			var position = queue[0][0];
+			var c = queue[0][1];
+			queue.shift();
+			//if(stableDiscs[position]==0 || stableDiscs[position]==3) continue;
+			stableDiscs[position] = c;
+			if( (stableDiscs[position-10]==3 || stableDiscs[position+10]==3  || stableDiscs[position-10] == c || stableDiscs[position+10] == c) &&
+				(stableDiscs[position-1]==3 || stableDiscs[position+1]==3  || stableDiscs[position-1] == c || stableDiscs[position+1] == c) &&
+				(stableDiscs[position-11]==3 || stableDiscs[position+11]==3  || stableDiscs[position-11] == c || stableDiscs[position+11] == c) &&
+				(stableDiscs[position-9]==3 || stableDiscs[position+9]==3  || stableDiscs[position-9] == c || stableDiscs[position+9] == c) )
+			{
+				stableDiscs[position]=c;
+				r += ((c==this.color)?1:-1)*7;
+				for(var i = 0;i <directions.length ; i++)
+					if(stableDiscs[directions[i]+position]==0 && this[directions[i]+position]==c)
+						queue.push([directions[i]+position,c]);
+			}
+		}*/		
+		
+		
+		return r;
+	}
+	this.toLocalString=function(depth)
+	{
+		var r="";
+		if(!depth)depth=0;
+		
+		for(var y=1;y<=8;y++)
+		{
+			for(var x=1;x<=8;x++)
+			{
+				if(this[y*10+x]!=0)r+=(this[y*10+x]==1?"*":"o")+" ";
+				else 
+				{
+					var tmp=this.move(x,y,this.color);
+					if(tmp)
+					{
+						var tmp2=-tmp.search(-Infinity,Infinity,depth);
+
+						if(tmp2<0||tmp2>9)r+=tmp2;
+						else r+=" "+tmp2;
+					}
+					else r+="X ";
+				}				 
+			}
+			r+="\n";
+		}
+		return r+this.exact();
+	}
+	this.computer=function(depth,exactDepth){
+		if(!depth)depth=0;
+		if(!exactDepth)exactDepth=depth;
+		var r=[];
+		var max=-Infinity;
+		for(var y=1;y<=8;y++)
+		{
+			for(var x=1;x<=8;x++)
+			{
+				if(this[y*10+x])continue;
+				else 
+				{
+					var tmp=this.move(x,y,this.color);
+					if(!tmp)continue;
+					if(this.moves+exactDepth>=60)
+					{
+						var v=-tmp.exactSearch(-Infinity,Infinity);
+						//alert([x,y]+":"+v);
+					}
+					else if( (x==2||x==7) && (y==2||y==7) )
+						var v=-tmp.search(-Infinity,Infinity,depth+1);
+					else
+						var v=-tmp.search(-Infinity,Infinity,depth);
+					if(v<max)continue;
+					if(v>max){
+						r=[[x,y]];max=v;
+					}
+					else r.push([x,y]);
+				}				 
+			}
+		}
+		var tmp=Math.floor(Math.random()*r.length);
+		return r[tmp];
+	}
+	this.search=function(alpha,beta,depth,pass){
+		if(depth==0)return this.calculate();		
+		var canmove=false;
+		for(var y=1;y<=8;y++)
+		{
+			for(var x=1;x<=8;x++)
+			{
+				if(this[y*10+x]!=0)r+=(this[y*10+x]==1?"*":"o")+" ";
+				else 
+				{
+					var tmp=this.move(x,y,this.color);
+					if(!tmp)continue;
+					canmove=true;
+					var r=-tmp.search(-beta,-alpha,depth-1);
+					//if(depth==4)WScript.echo(r);
+					if(r>=alpha)alpha=r;
+					if(alpha>beta)return Infinity;
+				}
+			}
+		}
+		if(canmove)return alpha;
+
+                if(pass) return this.exact();
+		this.color=3-this.color;
+		return -this.search(-beta,-alpha,depth-1,true);
+	}
+	this.exactSearch=function(alpha,beta,pass){
+		if(this.moves==60)return this.exact();
+		var canmove=false;
+		for(var y=1;y<=8;y++)
+		{
+			for(var x=1;x<=8;x++)
+			{
+				if(this[y*10+x]!=0);//r+=(this[y*10+x]==1?"*":"o")+" ";
+				else 
+				{
+					var tmp=this.move(x,y,this.color);
+					if(!tmp)continue;
+					canmove=true;
+					var r=-tmp.exactSearch(-beta,-alpha);
+					if(r>=alpha)alpha=r;
+					if(alpha>beta)return Infinity;
+				}
+			}
+		}
+
+		if(canmove)return alpha;
+		if(pass)return this.exact();
+		this.color=3-this.color;
+		return -this.exactSearch(-beta,-alpha,true);
+	}
+
+	this.move=function(x,y)
+	{
+		var pattern=this.clone();
+		pattern.color=3-this.color;
+		pattern.divergence=-pattern.divergence;
+		pattern.moves++;
+		var canmove;
+		canmove=false;
+		if(pattern[10*y+x]!=0)return null;
+		for(var i=0;i<8;i++)
+		{
+			var p=10*y+x+directions[i];
+			if(pattern[p]==3-this.color)
+				while(pattern[p]!=0)
+				{
+					p+=directions[i];			
+					if(pattern[p]==this.color)
+					{
+						canmove=true;
+						
+						while((p+=-directions[i])!=10*y+x)
+						{							
+							pattern[p]=this.color;
+							//alert(p);
+							for(var d=0;d<8;d++)
+							{								
+								if(!pattern[p+directions[d]]
+									&&p+directions[d]>10
+									&&p+directions[d]<89
+									&&(p+directions[d])%10!=0
+									&&(p+directions[d])!=9
+									&&p-directions[d]>10
+									&&p-directions[d]<89
+									&&(p-directions[d])%10!=0
+									&&(p-directions[d])!=9)pattern.divergence++;
+							}
+						}
+						break;
+					}
+				}
+		}
+		if(canmove){pattern[10*y+x]=this.color;return pattern;}
+		else return null;
+	}
+}
+//pattern.prototype = emptyboard;
+//WScript.echo(new pattern().move(5,6).move(6,4).move(4,3).move(3,4).toLocalString(3));
+//WScript.echo(new pattern().move(5,6).search(-Infinity,Infinity,2));
+
+
+}()
---
+++ src/Othello.js
@@ -0,0 +1,366 @@
+﻿
+void function() {
+    Othello = {};
+    Othello.OthelloAIPad = OthelloAIPad;
+    Othello.OthelloViewer = OthelloViewer;
+    Othello.OthelloPattern = OthelloPattern;
+    Othello.OthelloGame = OthelloGame;
+    Othello.OthelloBoard = OthelloBoard;
+    //    Othello.OthelloEditor = OthelloEditor;
+    //    Othello.OthelloViewer = OthelloViewer;
+
+    Othello.OthelloEditor
+
+    function OthelloPattern() {
+        this.pattern = new Array(100);
+        for (var i = 0; i < 100; i++) this.pattern[i] = 0;
+        with (this) {
+            this.clone = function() {
+                var ret = new OthelloPattern();
+                ret.pattern = pattern.slice();
+                return ret;
+            };
+            this.makeMove = function(x, y, c) {
+                var d = [-11, -10, -9, -1, 1, 9, 10, 11];
+                var turningPieces = [];
+                if (pattern[10 * x + y] == 0) {
+                    for (i = 0; i < 8; i++) {
+                        var p = 10 * x + y + d[i];
+                        if (pattern[p] == 3 - c) {
+                            while (pattern[p] != 0) {
+                                p += d[i];
+                                if (pattern[p] == c) {
+                                    turningPieces.push([p % 10, Math.floor(p / 10)])
+                                    while (p != 10 * x + y) {
+                                        p += -d[i];
+                                        pattern[p] = c;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if (turningPieces.length > 0) { pattern[10 * x + y] = c; }
+                }
+                return turningPieces;
+            };
+            this.getNextMovePattern = function(x, y) {
+
+            }
+            this.canMakeMove = function(c) {
+                var d = [-11, -10, -9, -1, 1, 9, 10, 11];
+                for (var y = 1; y <= 8; y++) {
+                    for (var x = 1; x <= 8; x++) {
+                        if (pattern[y * 10 + x] == 0) {
+                            for (i = 0; i < 8; i++) {
+                                var p = 10 * y + x + d[i];
+                                if (pattern[p] == 3 - c) {
+                                    while (pattern[p] != 0) {
+                                        p += d[i];
+                                        if (pattern[p] == c) {
+                                            return true;
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+                return false;
+            };
+
+            this.toString = function() {
+                var str = "";
+                for (var i = 1; i <= 8; i++) {
+                    for (var j = 1; j <= 8; j++) {
+                        if (pattern[i * 10 + j] == 0) str += " _";
+                        if (pattern[i * 10 + j] == 1) str += " o";
+                        if (pattern[i * 10 + j] == 2) str += " x";
+                    }
+                    str += "\n";
+                }
+                return str;
+            };
+        }
+    }
+    OthelloPattern.start = new OthelloPattern();
+    OthelloPattern.start.pattern[44] = 2;
+    OthelloPattern.start.pattern[45] = 1;
+    OthelloPattern.start.pattern[54] = 1;
+    OthelloPattern.start.pattern[55] = 2;
+
+
+    function OthelloGame(startPattern, moves) {
+        if (arguments.length == 0)
+            startPattern = OthelloPattern.start.clone();
+        else
+            startPattern = startPattern.clone();
+
+        this.patterns = new Array(startPattern);
+        this.moves = new Array;
+        this.colors = [1];
+        this.current = 0;
+
+
+        with (this) {
+            this.makeMove = function(x, y) {
+
+                var currentPattern = patterns[current];
+
+                var c;
+
+
+                c = colors[current];
+
+                var newPattern = currentPattern.clone();
+                if (c && newPattern.makeMove(x, y, c).length) {
+                    current++;
+                    this.moves.splice(current, Infinity);
+                    this.patterns.splice(current, Infinity);
+                    this.colors.splice(current, Infinity);
+                    moves.push([x, y]);
+                    patterns.push(newPattern);
+
+                    if (newPattern.canMakeMove(3 - c)) {
+                        colors.push(3 - c);
+                    }
+                    else if (newPattern.canMakeMove(c)) {
+                        colors.push(c);
+                    }
+                    else {
+                        colors.push(0);
+                    }
+                    return true;
+                }
+                else return false;
+
+            };
+            this.moveBackward = function(x, y) {
+                if (current > 0) current--;
+            }
+            this.moveForward = function() {
+                if (current < patterns.length - 1) current++;
+            };
+            this.moveToStart = function() {
+                current = 0;
+            };
+            this.moveToEnd = function() {
+                current = patterns.length - 1;
+            };
+            this.toString = function() {
+
+            };
+        }
+        if (moves && ((moves instanceof String) || (typeof moves == "string"))) {
+            for (var i = 0; i < moves.length; i++) {
+                var y = moves.charCodeAt(i++) - "a".charCodeAt(0) + 1;
+                var x = moves.charCodeAt(i) - "1".charCodeAt(0) + 1;
+                this.makeMove(x, y);
+            }
+        }
+    }
+
+    function OthelloBoard() {
+        var rootElement = null;
+        var pieceElements = [[], [], [], [], [], [], [], []];
+        with (this) {
+            this.getRootElement = function() {
+                return rootElement;
+            };
+            this.onBeforeRenderingPiece = function(x, y, element) {
+
+            };
+            this.applyToHTMLElement = function(div) {
+                rootElement = null;
+            };
+            this.createHTMLElement = function() {
+                rootElement = document.createElement("div");
+                rootElement.style.width = "297px";
+                rootElement.style.height = "297px";
+                var borderElement = document.createElement("div");
+                borderElement.style.cssText = "width:297px;height:21px;position:relative;overflow:hidden;float:left;";
+                borderElement.style.background = "url(\"Resources/u297x21.png\")";
+                rootElement.appendChild(borderElement);
+                for (i = 1; i <= 8; i++) {
+                    var borderElement = document.createElement("div");
+                    borderElement.style.cssText = "width:21px;height:32px;position:relative;overflow:hidden;float:left;";
+                    borderElement.style.background = "url(\"Resources/l" + i + "_21x32.png\")";
+                    rootElement.appendChild(borderElement);
+                    for (j = 1; j <= 8; j++) {
+                        var pieceElement = document.createElement("div");
+                        pieceElement.style.cssText = "width:32px;height:32px;position:relative;overflow:hidden;float:left;";
+                        pieceElement.background = "url(\"Resources/b31.png\")";
+                        this.onBeforeRenderingPiece(j, i, pieceElement);
+                        rootElement.appendChild(pieceElement);
+                        pieceElements[i - 1][j - 1] = pieceElement;
+                        pieceElements[i - 1][j - 1].onclick = (function(x, y) {
+                            return function() { onSquareClick(x, y) }
+                        })(i, j);
+                    }
+                    var borderElement = document.createElement("div");
+                    borderElement.style.cssText = "width:20px;height:32px;position:relative;overflow:hidden;float:left;";
+                    borderElement.style.background = "url(\"Resources/r" + i + "_20x32.png\")";
+                    rootElement.appendChild(borderElement);
+                }
+                var borderElement = document.createElement("div");
+                borderElement.style.cssText = "width:297px;height:20px;position:relative;overflow:hidden;float:left;";
+                borderElement.style.background = "url(\"Resources/d297x20.png\")";
+                rootElement.appendChild(borderElement);
+
+                return rootElement;
+            };
+            this.onSquareClick = function(x, y) {
+                //to be override
+            }
+            this.loadOthelloPattern = function(game) {
+                for (var i = 0; i < 8; i++) {
+                    for (var j = 0; j < 8; j++) {
+                        switch (game.pattern[(i + 1) * 10 + j + 1]) {
+                            case 1:
+                                pieceElements[i][j].style.background = "url(\"Resources/b31.png\")";
+                                break;
+                            case 2:
+                                pieceElements[i][j].style.background = "url(\"Resources/w31.png\")";
+                                break;
+                            default:
+                                pieceElements[i][j].style.background = "url(\"Resources/e31.png\")";
+                                break;
+                        }
+                    }
+                }
+            };
+            this.putPiece = function(x, y) {
+
+            };
+            this.turnPiece = function(x, y) {
+
+            };
+            this.turnPieces = function(pieces) {
+
+            };
+        }
+    }
+    function OthelloViewer(game) {
+        this.board = new OthelloBoard(this);
+        if (!(game && game instanceof OthelloGame))
+            game = new OthelloGame();
+        var color = 1;
+        var rootElement = null;
+        game.moveToStart();
+        with (this) {
+            this.getRootElement = function() {
+                return rootElement;
+            };
+            this.createHTMLElement = function() {
+                rootElement = document.createElement("div");
+                rootElement.style.width = "297px";
+                rootElement.style.height = "327px";
+                rootElement.style.position = "relative";
+                this.boardElement = board.createHTMLElement();
+                board.loadOthelloPattern(game.patterns[game.current]);
+                rootElement.appendChild(boardElement);
+
+                this.controlElement = document.createElement("div");
+                controlElement.style.width = "297px";
+                controlElement.style.height = "30px";
+                controlElement.style.background = "#cccccc";
+                controlElement.style.position = "relative";
+                controlElement.style.textAlign = "center"
+                rootElement.appendChild(controlElement);
+
+                this.toStartButton = document.createElement("a");
+                toStartButton.innerHTML = "&lt;&lt;";
+                toStartButton.style.margin = "20px";
+                toStartButton.href = "javascript:void 0;";
+                toStartButton.onclick = function() {
+                    game.moveToStart();
+                    board.loadOthelloPattern(game.patterns[game.current]);
+                }
+                this.backwardButton = document.createElement("a");
+                backwardButton.innerHTML = "&lt;";
+                backwardButton.style.margin = "20px";
+                backwardButton.href = "javascript:void 0;";
+                backwardButton.onclick = function() {
+                    game.moveBackward();
+                    board.loadOthelloPattern(game.patterns[game.current]);
+                }
+                this.forwardButton = document.createElement("a");
+                forwardButton.innerHTML = "&gt;";
+                forwardButton.style.margin = "20px";
+                forwardButton.href = "javascript:void 0;";
+                forwardButton.onclick = function() {
+                    game.moveForward();
+                    board.loadOthelloPattern(game.patterns[game.current]);
+                }
+                this.toEndButton = document.createElement("a");
+                toEndButton.innerHTML = "&gt;&gt;";
+                toEndButton.style.margin = "20px";
+                toEndButton.href = "javascript:void 0;";
+                toEndButton.onclick = function() {
+                    game.moveToEnd();
+                    board.loadOthelloPattern(game.patterns[game.current]);
+                }
+                controlElement.appendChild(toStartButton);
+                controlElement.appendChild(backwardButton);
+                controlElement.appendChild(forwardButton);
+                controlElement.appendChild(toEndButton);
+                return rootElement;
+            }
+            this.loadOthelloGame = function() {
+            }
+        }
+    }
+    function OthelloAIPad(game) {
+        OthelloBoard.call(this);
+        var game = game || new OthelloGame();
+        var color = 1;
+        var _super = { createHTMLElement: this.createHTMLElement };
+        var ai = new (AI.Pattern);
+        var thinking = false;
+        this.createHTMLElement = function() {
+            var rootElement = _super.createHTMLElement.apply(this);
+            this.loadOthelloPattern(game.patterns[game.current]);
+            return rootElement;
+        }
+        this.onMove = function(x, y, color) {
+        }
+        this.onSquareClick = function(x, y) {
+            if(thinking) return;
+            thinking = true;
+            if (game.makeMove(x, y)) {
+                ai=ai.move(y,x);
+                var me = this;
+                this.loadOthelloPattern(game.patterns[game.current]);
+                if(!ai.pass())
+                {                
+                    setTimeout(function(){ me.computerMove(); },10);
+                }
+                else thinking = false;
+            }
+            else thinking = false;
+        };
+window.thinkingTime = 0;
+        this.computerMove= function() {
+
+var begin = new Date();
+            var t=ai.computer(3,10);
+window.thinkingTime += new Date - begin;
+            game.makeMove(t[1], t[0]);
+            ai=ai.move(t[0],t[1]);
+            var me = this;
+            if(ai.pass())
+            {
+                setTimeout(function(){ me.computerMove(); },500);
+            }
+            else thinking = false;
+            this.loadOthelloPattern(game.patterns[game.current]);
+        };
+    }
+    //window.onload = function() {
+    //    var board = (new OthelloViewer());
+    //    board.createHTMLElement();
+    //    document.body.appendChild(board.getRootElement());
+    //}
+} ();
+
